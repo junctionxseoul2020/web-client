dc37e87ddbb891b531fb5071be1836634a0226be
DDark <ddark.kr@gmail.com>
2020-10-10 07:20:54 +0900

    style: conference button (?)

--- src/components/layouts/default/parts/sidebar__parts/conference.tsx
+++ src/components/layouts/default/parts/sidebar__parts/conference.tsx
@@ -7,20 +7,28 @@ interface ConferenceProps {
 }
 
 const ConferenceComponent: React.FC<ConferenceProps> = () => {
-  const ConferenceWrapper = styled.div``;
+  const ConferenceWrapper = styled.div`
+    text-align: center;
+  `;
   const ConferenceCircle = styled.div`
     width: 54px;
     height: 54px;
+    border-radius: 50%;
     box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
     background: var(--white);
     color: var(--primary);
   `;
-  const ConferenceTime = styled.div`
+  const ConferenceTime = styled.span`
+    display: inline-block;
+    line-height: 1.5;
     width: 44px;
     height: 20px;
     border-radius: 4px;
     background-color: var(--primary);
     color: #fff;
+    font-size: 12px;
+    font-weight: 600;
+    margin-top: 8px;
   `;
   return (
     <ConferenceWrapper>
