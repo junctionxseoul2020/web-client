8de6a89421ab59b8c43be4af5f5a0c3d0e0445d7
DDark <ddark.kr@gmail.com>
2020-10-10 02:12:43 +0900

    feat: Index page (Hello world!)

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const IndexPage: React.FC = () => {
+  const HelloWorld = styled.div`
+    padding: 20px;
+  `;
+
+  return <HelloWorld>Hello world!</HelloWorld>;
+};
+
+export default IndexPage;
