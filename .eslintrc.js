ab2cfac7ab9f451fc9682491c0c18fae77915238
DDark <ddark.kr@gmail.com>
2020-10-10 01:52:31 +0900

    chore: 개발 환경 설정 (ESLint, Prettier 등)

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'react-app',
+    'plugin:prettier/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'jsx-a11y', 'emotion'],
+  rules: {},
+};
