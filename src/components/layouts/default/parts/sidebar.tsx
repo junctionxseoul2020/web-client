c4cd918c2f36e80b4b9cd89d9a42f142b685f9e9
DDark <ddark.kr@gmail.com>
2020-10-10 07:42:19 +0900

    feat: SidebarBottom

--- src/components/layouts/default/parts/sidebar.tsx
+++ src/components/layouts/default/parts/sidebar.tsx
@@ -8,12 +8,23 @@ const SidebarComponent: React.FC = () => {
     height: calc(100vh - 38px);
     background: var(--secondary);
     padding: 24px 0;
+    position: relative;
+  `;
+
+  const SidebarBottom = styled.div`
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
   `;
 
   return (
     <SidebarWrapper>
       <ConferenceComponent now={true} />
       <ConferenceComponent time="12:00" />
+      <SidebarBottom>
+        <ConferenceComponent />
+      </SidebarBottom>
     </SidebarWrapper>
   );
 };
