648998d0dca007c65ade6444bc101c6dd9666ba9
DDark <ddark.kr@gmail.com>
2020-10-10 19:59:04 +0900

    feat: ChatInputComponent 생성

---
+++ src/components/chats/input.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const ChatInputComponent: React.FC = () => {
+  const InputWrapper = styled.div``;
+
+  return <InputWrapper></InputWrapper>;
+};
+
+export default ChatInputComponent;
