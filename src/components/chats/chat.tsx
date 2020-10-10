d08422d84de956a9957bc6a7531842d2ede1b387
DDark <ddark.kr@gmail.com>
2020-10-10 18:00:29 +0900

    feat: chat page

---
+++ src/components/chats/chat.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+interface ChatComponentProps {
+  nickname: string;
+  time: string;
+  content: string | React.ReactNode;
+}
+
+const ChatComponent: React.FC<ChatComponentProps> = props => {
+  const ChatWrapper = styled.div`
+    display: grid;
+    grid-template-columns: 36px auto;
+    grid-column-gap: 10px;
+    padding: 14px 18px;
+  `;
+
+  const ChatProfileImg = styled.img`
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+    box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+  `;
+
+  const ChatNickname = styled.span`
+    color: var(--dark);
+    font-size: 15px;
+    font-weight: 600;
+  `;
+  const ChatTime = styled.span`
+    color: var(--grey);
+    font-size: 12px;
+    font-weight: 400;
+    margin-left: 14px;
+  `;
+  const ChatContent = styled.div`
+    font-size: 15px;
+    line-height: 1.6;
+    color: var(--dark);
+    margin-top: 4px;
+  `;
+
+  return (
+    <ChatWrapper>
+      <ChatProfileImg src="https://via.placeholder.com/150" />
+      <div>
+        <div>
+          <ChatNickname>{props.nickname}</ChatNickname>
+          <ChatTime>{props.time}</ChatTime>
+        </div>
+        <ChatContent>{props.content}</ChatContent>
+      </div>
+    </ChatWrapper>
+  );
+};
+
+export default ChatComponent;
--- src/pages/chats/[id].tsx
+++ src/pages/chats/[id].tsx
@@ -3,12 +3,23 @@ import React from 'react';
 import ChatLayout from '@/components/layouts/chat';
 import IntroBox from '@/components/chats/introBox';
 import ChatSeperator from '@/components/chats/seperator';
+import ChatComponent from '@/components/chats/chat';
 
 const ChatPage: React.FC = () => {
   return (
     <ChatLayout>
       <IntroBox title="#channel_name" desc="이곳에 무엇이 들어갈지 적어주세요" />
       <ChatSeperator />
+      <ChatComponent nickname="강희원" time="12:00AM" content={'컨텐츠는 string이나,'} />
+      <ChatComponent
+        nickname="도다"
+        time="12:00AM"
+        content={
+          <>
+            <pre>JSX</pre>가 들어갈 수도 있어요. <b>bold</b> <i>italic</i>
+          </>
+        }
+      />
     </ChatLayout>
   );
 };
