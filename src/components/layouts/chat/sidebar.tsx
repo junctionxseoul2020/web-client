3d2d4534ecb162165c666204753df6ffec6a5e9a
DDark <ddark.kr@gmail.com>
2020-10-10 15:23:55 +0900

    feat: chat 레이아웃 사이드바 구성

--- src/components/layouts/chat/sidebar.tsx
+++ src/components/layouts/chat/sidebar.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import CdownIcon from '@/components/icons/cdown';
+import HeadLinkItem from './sidebar__parts/headLink';
+import MoreIcon from '@/components/icons/more';
 
 const Sidebar: React.FC = () => {
   const SidebarWrapper = styled.div`
@@ -12,17 +15,34 @@ const Sidebar: React.FC = () => {
     height: 64px;
     border-bottom: 1px solid #e0e2ea;
     padding: 0 16px;
+    svg {
+      margin-left: 10px;
+    }
   `;
   const Title = styled.span`
     font-size: 15px;
     font-weight: 700;
   `;
 
+  const ContentWrapper = styled.div`
+    padding: 11px 0;
+  `;
+
   return (
     <SidebarWrapper>
       <TitleSection>
         <Title>JunctionX Seoul 2020</Title>
+        <CdownIcon />
       </TitleSection>
+      <ContentWrapper>
+        <HeadLinkItem>Threads</HeadLinkItem>
+        <HeadLinkItem>All DMs</HeadLinkItem>
+        <HeadLinkItem>Drafts</HeadLinkItem>
+        <HeadLinkItem>Mentions & reactions</HeadLinkItem>
+        <HeadLinkItem fontWeight="normal">
+          <MoreIcon /> More
+        </HeadLinkItem>
+      </ContentWrapper>
     </SidebarWrapper>
   );
 };
