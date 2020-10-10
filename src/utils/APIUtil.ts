b48108ec3b5b0774fedb7ed7d4233b2b3e99bf35
kang-heewon <heewon.dev@gmail.com>
2020-10-11 08:50:11 +0900

    feat: login & register

--- src/utils/APIUtil.ts
+++ src/utils/APIUtil.ts
@@ -1,10 +1,6 @@
+import axios from 'axios';
+
 export async function requestAPI<T>(url: string, request?: unknown): Promise<T> {
-  const res = await fetch(`https://junctionx.azurewebsites.net${url}`, {
-    method: 'POST',
-    body: JSON.stringify(request),
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-    },
-  });
-  return await res.json();
+  const response = await axios.post<T>(`http://junctionx.azurewebsites.net${url}`, request);
+  return response.data;
 }
