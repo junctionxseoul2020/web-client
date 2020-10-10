90ff2f322498821f3e153589693117b3f02ea7dc
DDark <ddark.kr@gmail.com>
2020-10-10 17:09:04 +0900

    feat: Chat Seperator

--- src/components/chats/introBox.tsx
+++ src/components/chats/introBox.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-const IntroBox = () => {
+const IntroBox: React.FC<{ title?: string; desc?: string }> = props => {
   const IntroWrapper = styled.div`
     padding: 42px 18px;
     h1 {
       font-size: 28px;
       line-height: 33px;
-      margin: 0;
     }
     p {
       color: var(--primary);
       font-size: 15px;
-      margin: 0;
+      margin-top: 18px;
     }
   `;
   return (
     <IntroWrapper>
-      <h1>#channel_name</h1>
-      <p>asd</p>
+      <h1>{props.title || '#unknown'}</h1>
+      {props.desc && <p>{props.desc}</p>}
     </IntroWrapper>
   );
 };
--- src/pages/chats/[id].tsx
+++ src/pages/chats/[id].tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import ChatLayout from '@/components/layouts/chat';
 import IntroBox from '@/components/chats/introBox';
+import ChatSeperator from '@/components/chats/seperator';
 
 const ChatPage: React.FC = () => {
   return (
     <ChatLayout>
-      <IntroBox />
+      <IntroBox title="#channel_name" desc="이곳에 무엇이 들어갈지 적어주세요" />
+      <ChatSeperator />
     </ChatLayout>
   );
 };
