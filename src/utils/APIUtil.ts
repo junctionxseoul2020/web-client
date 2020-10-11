958c3facdd902c003e48948dd74fd47c1fdc2486
kang-heewon <heewon.dev@gmail.com>
2020-10-11 09:09:35 +0900

    fix: api url & register 페이지 이동

--- src/utils/APIUtil.ts
+++ src/utils/APIUtil.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export async function requestAPI<T>(url: string, request?: unknown): Promise<T> {
-  const response = await axios.post<T>(`http://junctionx.azurewebsites.net${url}`, request);
+  const response = await axios.post<T>(`https://junctionx.azurewebsites.net${url}`, request);
   return response.data;
 }
