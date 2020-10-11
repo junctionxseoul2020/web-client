ba98e309d9794697e44cc540f70696a4748f0741
DDark <ddark.kr@gmail.com>
2020-10-11 10:28:37 +0900

    refactor: styled 위치

--- src/components/chats/chat.tsx
+++ src/components/chats/chat.tsx
@@ -8,38 +8,6 @@ interface ChatComponentProps {
 }
 
 const ChatComponent: React.FC<ChatComponentProps> = React.memo(props => {
-  const ChatWrapper = styled.div`
-    display: grid;
-    grid-template-columns: 36px auto;
-    grid-column-gap: 10px;
-    padding: 14px 18px;
-  `;
-
-  const ChatProfileImg = styled.img`
-    width: 36px;
-    height: 36px;
-    border-radius: 50%;
-    box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
-  `;
-
-  const ChatNickname = styled.span`
-    color: var(--dark);
-    font-size: 15px;
-    font-weight: 600;
-  `;
-  const ChatTime = styled.span`
-    color: var(--grey);
-    font-size: 12px;
-    font-weight: 400;
-    margin-left: 14px;
-  `;
-  const ChatContent = styled.div`
-    font-size: 15px;
-    line-height: 1.6;
-    color: var(--dark);
-    margin-top: 4px;
-  `;
-
   return (
     <ChatWrapper>
       <ChatProfileImg src="https://via.placeholder.com/150" />
@@ -53,5 +21,37 @@ const ChatComponent: React.FC<ChatComponentProps> = React.memo(props => {
     </ChatWrapper>
   );
 });
+
+const ChatWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 36px auto;
+  grid-column-gap: 10px;
+  padding: 14px 18px;
+`;
+
+const ChatProfileImg = styled.img`
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+`;
+
+const ChatNickname = styled.span`
+  color: var(--dark);
+  font-size: 15px;
+  font-weight: 600;
+`;
+const ChatTime = styled.span`
+  color: var(--grey);
+  font-size: 12px;
+  font-weight: 400;
+  margin-left: 14px;
+`;
+const ChatContent = styled.div`
+  font-size: 15px;
+  line-height: 1.6;
+  color: var(--dark);
+  margin-top: 4px;
+`;
 
 export default ChatComponent;
